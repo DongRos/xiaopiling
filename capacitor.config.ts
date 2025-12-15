8d83c402295ef5e8c75642dff8f75c0b866246cb
DongRos <163140596+DongRos@users.noreply.github.com>
2025-12-15 20:35:49 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.purrfectlove.app',
-  appName: '小屁铃',
-  webDir: 'dist'
+  appName: '我们的小窝',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https' // 建议加上这个，让安卓也使用 https 协议运行本地服务
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true, // 开启原生 HTTP 插件，接管请求
+    },
+  },
 };
 
 export default config;
